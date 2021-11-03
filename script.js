5429ddf79bcaf45e6ec7ad45549dd4021dd98f84
Ron Sanga <93544882+ronsanga@users.noreply.github.com>
2021-11-03 14:20:00 -0400

    Final project upload

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+function compute() {
+    var principal = document.getElementById("principal").value;
+    if (principal<=0) {
+        document.getElementById("principal").focus();
+        alert("enter a positive number");
+    }
+    else {
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+    var interest = principal * years * rate /100;
+    var year = new Date().getFullYear()+parseInt(years);
+    var ans=document.getElementById("result");
+    ans.innerHTML="If you deposit <mark>" + principal + "</mark>,<br/>" + " at an intrest rate of <mark>" + rate + "</mark>%.<br>" + "you will receive an amount of <mark>" + interest + "</mark>,<br>" + "in the year <mark>" + year + "</mark>";
+    }
 }
-        
+
+function newintrest() {
+    var ans=document.getElementById("rate_val");
+    ans.innerHTML=document.getElementById("rate").value+"%";
+}
